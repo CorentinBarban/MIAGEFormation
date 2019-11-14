f29c2952564635d5f5443c34e8d0bc9665b78b41
maths <maths@DESKTOP-J3UCU50>
2019-11-14 22:09:57 +0100

    création de l'interface et des différentes méthodes métier pour la gestion commerciale

---
+++ MIAGECommercial/MIAGECommercial-ejb/src/main/java/com/barban/corentin/commercial/business/gestionCommercialeLocal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.barban.corentin.commercial.business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author maths
+ */
+public interface gestionCommercialeLocal {
+    
+    void recupererCatalogueFormations(); 
+    
+    void memoriserDemandeFormation(int code, int numClient, Date dateFor);
+    
+    void editerCompteRendus();
+    
+    void demanderStatutFormation(int idFormation);
+    
+}
