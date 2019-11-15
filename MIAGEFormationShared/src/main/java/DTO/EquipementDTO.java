054fa83be2723e625f6565385b88b4643d9c4bdf
Corentin <$Blender1>
2019-11-15 14:25:50 +0100

    Modify SalleDTO Add CalendrierDTO add Equipement DTO

---
+++ MIAGEFormationShared/src/main/java/DTO/EquipementDTO.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author Corentin
+ */
+public class EquipementDTO {
+    
+    private Integer idequipement;
+    private String nomequipement;
+    private Collection<SalleDTO> salleCollection;
+
+    public Integer getIdequipement() {
+        return idequipement;
+    }
+
+    public void setIdequipement(Integer idequipement) {
+        this.idequipement = idequipement;
+    }
+
+    public String getNomequipement() {
+        return nomequipement;
+    }
+
+    public void setNomequipement(String nomequipement) {
+        this.nomequipement = nomequipement;
+    }
+
+    public Collection<SalleDTO> getSalleCollection() {
+        return salleCollection;
+    }
+
+    public void setSalleCollection(Collection<SalleDTO> salleCollection) {
+        this.salleCollection = salleCollection;
+    }
+
+    @Override
+    public String toString() {
+        return "EquipementDTO{" + "idequipement=" + idequipement + ", nomequipement=" + nomequipement + ", salleCollection=" + salleCollection + '}';
+    }
+    
+    
+    
+}
--- MIAGEFormationShared/src/main/java/DTO/SalleDTO.java
+++ MIAGEFormationShared/src/main/java/DTO/SalleDTO.java
@@ -5,10 +5,55 @@
  */
 package DTO;
 
+import java.util.Collection;
+
 /**
  *
  * @author Corentin
  */
 public class SalleDTO {
+    
+    private Integer idsalle;
+    private String nom;
+    private Collection<EquipementDTO> equipementCollection;
+    private Collection<CalendrierSalleDTO> calendrierSalleCollection;
+
+    public Integer getIdsalle() {
+        return idsalle;
+    }
+
+    public void setIdsalle(Integer idsalle) {
+        this.idsalle = idsalle;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Collection<EquipementDTO> getEquipementCollection() {
+        return equipementCollection;
+    }
+
+    public void setEquipementCollection(Collection<EquipementDTO> equipementCollection) {
+        this.equipementCollection = equipementCollection;
+    }
+
+    public Collection<CalendrierSalleDTO> getCalendrierSalleCollection() {
+        return calendrierSalleCollection;
+    }
+
+    public void setCalendrierSalleCollection(Collection<CalendrierSalleDTO> calendrierSalleCollection) {
+        this.calendrierSalleCollection = calendrierSalleCollection;
+    }
+
+    @Override
+    public String toString() {
+        return "SalleDTO{" + "idsalle=" + idsalle + ", nom=" + nom + ", equipementCollection=" + equipementCollection + ", calendrierSalleCollection=" + calendrierSalleCollection + '}';
+    }
+    
     
 }
