1afa61c526c9abe1f23fceabc37cddd7228fe812
jdetr <jdetr@DESKTOP-S0BR4T9>
2019-11-15 15:32:40 +0100

    fin de l'interface local

--- MIAGETechnicoCommercial/MIAGETechnicoCommercial-ejb/src/main/java/com/barban/corentin/technicoCommercial/businesses/gestionTechnicoCommerciale.java
+++ MIAGETechnicoCommercial/MIAGETechnicoCommercial-ejb/src/main/java/com/barban/corentin/technicoCommercial/businesses/gestionTechnicoCommerciale.java
@@ -5,10 +5,22 @@
  */
 package com.barban.corentin.technicoCommercial.businesses;
 
+import Exceptions.FormateurNotFoundException;
+import Exceptions.FormationCatalogueException;
+import Exceptions.FormationCatalogueNotFoundException;
+import Exceptions.LienFormateurFormationException;
+import Exceptions.LienFormateurFormationNotFoundException;
+import Exceptions.LienSalleFormationException;
+import Exceptions.LienSalleFormationNotFoundException;
+import Exceptions.SalleNotFoundException;
 import com.barban.corentin.technicoCommercial.entities.Formateurcompetent;
 import com.barban.corentin.technicoCommercial.entities.Formationcatalogue;
 import com.barban.corentin.technicoCommercial.entities.Salleadequate;
+import com.barban.corentin.technicoCommercial.repositories.FormateurcompetentFacade;
+import com.barban.corentin.technicoCommercial.repositories.FormationcatalogueFacade;
+import com.barban.corentin.technicoCommercial.repositories.SalleadequateFacade;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 /**
@@ -18,48 +30,61 @@ import javax.ejb.Stateless;
 @Stateless
 public class gestionTechnicoCommerciale implements gestionTechnicoCommercialeLocal {
 
+    @EJB 
+    private FormateurcompetentFacade formateurF;
+    
+    @EJB
+    private FormationcatalogueFacade formationF;
+    
+    @EJB
+    private SalleadequateFacade salleF;
+
     @Override
-    public Formationcatalogue ajouterFormationCatalogue(String code, String intitule, String niveau, String typeduree, Integer capacitemin, Integer capacitemax, Double tarifforfaitaire) {
+    public Formationcatalogue ajouterFormationCatalogue(String code, String intitule, String niveau, String typeduree, Integer capacitemin, Integer capacitemax, Double tarifforfaitaire) throws FormationCatalogueException {
+        if (this.formationF.find(code) != null) {
+            throw new FormationCatalogueException();
+        }
+        return null;
+        
+    }
+
+    @Override
+    public boolean supprimerFormationCatalogue(String code) throws FormationCatalogueNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean supprimerFormationCatalogue(String code) {
+    public Formationcatalogue consulterFormationCatalogue(String code) throws FormationCatalogueNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Formationcatalogue consulterFormationCatalogue(String code) {
+    public boolean ajouterFormateurAFormation(String code, int formateurkey) throws FormationCatalogueNotFoundException, FormateurNotFoundException, LienFormateurFormationException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean ajouterFormateurAFormation(String code, int formateurkey) {
+    public boolean supprimerFormateurDeFormation(String code, int formateurkey) throws FormationCatalogueNotFoundException, FormateurNotFoundException, LienFormateurFormationNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean supprimerFormateurDeFormation(String code, int formateurkey) {
+    public List<Formateurcompetent> rechercherFormateursDeFormation(String code) throws FormationCatalogueNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List<Formateurcompetent> rechercherFormateursDeFormation(String code) {
+    public boolean ajouterSalleAFormation(String code, int sallekey) throws FormationCatalogueNotFoundException, SalleNotFoundException, LienSalleFormationException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean ajouterSalleAFormation(String code, int sallekey) {
+    public boolean supprimerSalleDeFormation(String code, int sallekey) throws FormationCatalogueNotFoundException, SalleNotFoundException, LienSalleFormationNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean supprimerSalleDeFormation(String code, int sallekey) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<Salleadequate> rechercherSallesDeFormation(String code) {
+    public List<Salleadequate> rechercherSallesDeFormation(String code) throws FormationCatalogueNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -72,7 +97,6 @@ public class gestionTechnicoCommerciale implements gestionTechnicoCommercialeLoc
     public boolean validerExistenceFormation(String code) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    
+    
 }
--- MIAGETechnicoCommercial/MIAGETechnicoCommercial-ejb/src/main/java/com/barban/corentin/technicoCommercial/businesses/gestionTechnicoCommercialeLocal.java
+++ MIAGETechnicoCommercial/MIAGETechnicoCommercial-ejb/src/main/java/com/barban/corentin/technicoCommercial/businesses/gestionTechnicoCommercialeLocal.java
@@ -5,6 +5,14 @@
  */
 package com.barban.corentin.technicoCommercial.businesses;
 
+import Exceptions.FormateurNotFoundException;
+import Exceptions.FormationCatalogueException;
+import Exceptions.FormationCatalogueNotFoundException;
+import Exceptions.LienFormateurFormationException;
+import Exceptions.LienFormateurFormationNotFoundException;
+import Exceptions.LienSalleFormationException;
+import Exceptions.LienSalleFormationNotFoundException;
+import Exceptions.SalleNotFoundException;
 import com.barban.corentin.technicoCommercial.entities.Formateurcompetent;
 import com.barban.corentin.technicoCommercial.entities.Formationcatalogue;
 import com.barban.corentin.technicoCommercial.entities.Salleadequate;
@@ -28,25 +36,25 @@ public interface gestionTechnicoCommercialeLocal {
      * @param capacitemax
      * @param tarifforfaitaire
      * @return la formation créée
-     * @throws PrimaryKeyFormationCatalogueException 
+     * @throws FormationCatalogueException 
      */
-    Formationcatalogue ajouterFormationCatalogue(String code, String intitule, String niveau, String typeduree, Integer capacitemin, Integer capacitemax, Double tarifforfaitaire); //throws PrimaryKeyFormationCatalogueException ; 
+    Formationcatalogue ajouterFormationCatalogue(String code, String intitule, String niveau, String typeduree, Integer capacitemin, Integer capacitemax, Double tarifforfaitaire) throws FormationCatalogueException ; 
  
     /**
      * Supprimer une formation du catalogue
      * @param code
      * @return booléen de vérification
-     * @throws NotFoundException 
+     * @throws FormationCatalogueNotFoundException 
      */
-    boolean supprimerFormationCatalogue(String code); //throws NotFoundException ;
+    boolean supprimerFormationCatalogue(String code) throws FormationCatalogueNotFoundException ;
     
     /**
      * Rechercher une formation dans le catalogue
      * @param code
      * @return la formation trouvée
-     * @throws NotFoundException 
+     * @throws FormationCatalogueNotFoundException 
      */
-    Formationcatalogue consulterFormationCatalogue(String code); //throws NotFoundException ; 
+    Formationcatalogue consulterFormationCatalogue(String code) throws FormationCatalogueNotFoundException ; 
 
     /**
      * Ajouter un formateur compétent pour une formation
@@ -54,10 +62,10 @@ public interface gestionTechnicoCommercialeLocal {
      * @param code
      * @return booléen de vérification
      * @throws LienFormateurFormationException 
-     * @throws NotFoundException
+     * @throws FormationCatalogueNotFoundException
      * @throws FormateurNotFoundException
      */
-    boolean ajouterFormateurAFormation(String code, int formateurkey); //throws NotFoundException, FormateurNotFoundException, LienFormateurFormationException ;
+    boolean ajouterFormateurAFormation(String code, int formateurkey) throws FormationCatalogueNotFoundException, FormateurNotFoundException, LienFormateurFormationException ;
    
     /**
      * Supprimer un formateur d'une formation
@@ -65,18 +73,18 @@ public interface gestionTechnicoCommercialeLocal {
      * @param code
      * @return booléen de vérification
      * @throws LienFormateurFormationNotFoundException 
-     * @throws NotFoundException
+     * @throws FormationCatalogueNotFoundException
      * @throws FormateurNotFoundException
      */
-    boolean supprimerFormateurDeFormation(String code, int formateurkey); //throws NotFoundException, FormateurNotFoundException, LienFormateurFormationNotFoundException ;
+    boolean supprimerFormateurDeFormation(String code, int formateurkey) throws FormationCatalogueNotFoundException, FormateurNotFoundException, LienFormateurFormationNotFoundException ;
    
     /**
      * Lister les formateurs compétents pour une formation donnée
      * @param code
      * @return la liste des formateurs compétents pour la formation donnée
-     * @throws NotFoundException
+     * @throws FormationCatalogueNotFoundException
      */
-    List<Formateurcompetent> rechercherFormateursDeFormation(String code); //throws NotFoundException ;
+    List<Formateurcompetent> rechercherFormateursDeFormation(String code) throws FormationCatalogueNotFoundException ;
 
         /**
      * Ajouter une salle adéquate pour une formation
@@ -84,10 +92,10 @@ public interface gestionTechnicoCommercialeLocal {
      * @param code
      * @return booléen de vérification
      * @throws LienSalleFormationException 
-     * @throws NotFoundException
+     * @throws FormationCatalogueNotFoundException
      * @throws SalleNotFoundException
      */
-    boolean ajouterSalleAFormation(String code, int sallekey); //throws NotFoundException, SalleNotFoundException, LienSalleFormationException ;
+    boolean ajouterSalleAFormation(String code, int sallekey) throws FormationCatalogueNotFoundException, SalleNotFoundException, LienSalleFormationException ;
    
     /**
      * Supprimer une salle d'une formation
@@ -95,18 +103,18 @@ public interface gestionTechnicoCommercialeLocal {
      * @param code
      * @return booléen de vérification
      * @throws LienSalleFormationNotFoundException 
-     * @throws NotFoundException
+     * @throws FormationCatalogueNotFoundException
      * @throws SalleNotFoundException
      */
-    boolean supprimerSalleDeFormation(String code, int sallekey); //throws NotFoundException, SalleNotFoundException, LienSalleFormationNotFoundException ;
+    boolean supprimerSalleDeFormation(String code, int sallekey) throws FormationCatalogueNotFoundException, SalleNotFoundException, LienSalleFormationNotFoundException ;
    
     /**
      * Lister les salles adéquates pour une formation donnée
      * @param code
      * @return la liste des salles adéquates pour la formation donnée
-     * @throws NotFoundException
+     * @throws FormationCatalogueNotFoundException
      */
-    List<Salleadequate> rechercherSallesDeFormation(String code); //throws NotFoundException ;
+    List<Salleadequate> rechercherSallesDeFormation(String code) throws FormationCatalogueNotFoundException ;
 
     /**
      * Lister les formations du catalogue
