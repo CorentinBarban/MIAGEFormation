b9d6850745308ec345a2f4edf7db30a2c0d53b49
maths <math.stiv@gmail.com>
2019-11-15 14:07:18 +0100

    Création de FormationDTO

--- MIAGEFormationShared/src/main/java/DTO/FormationDTO.java
+++ MIAGEFormationShared/src/main/java/DTO/FormationDTO.java
@@ -5,10 +5,116 @@
  */
 package DTO;
 
+import java.util.Date;
+
 /**
  *
  * @author Corentin
  */
 public class FormationDTO {
     
+    Integer idformation;
+    String intitule;
+    String nomclient;
+    int nbpersonne;
+    String statut;
+    Date dateformation;
+    int keyformationcatalogue;
+    int keyformateur;
+    int keysalle;
+    int keydemandeformation;
+    
+    
+    public FormationDTO() {
+    }
+
+    public FormationDTO(Integer idformation) {
+        this.idformation = idformation;
+    }
+
+    public Integer getIdformation() {
+        return idformation;
+    }
+
+    public void setIdformation(Integer idformation) {
+        this.idformation = idformation;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+
+    public String getNomclient() {
+        return nomclient;
+    }
+
+    public void setNomclient(String nomclient) {
+        this.nomclient = nomclient;
+    }
+
+    public Integer getNbpersonne() {
+        return nbpersonne;
+    }
+
+    public void setNbpersonne(Integer nbpersonne) {
+        this.nbpersonne = nbpersonne;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    public Date getDateformation() {
+        return dateformation;
+    }
+
+    public void setDateformation(Date dateformation) {
+        this.dateformation = dateformation;
+    }
+
+    public Integer getKeyformationcatalogue() {
+        return keyformationcatalogue;
+    }
+
+    public void setKeyformationcatalogue(Integer keyformationcatalogue) {
+        this.keyformationcatalogue = keyformationcatalogue;
+    }
+
+    public Integer getKeyformateur() {
+        return keyformateur;
+    }
+
+    public void setKeyformateur(Integer keyformateur) {
+        this.keyformateur = keyformateur;
+    }
+
+    public Integer getKeysalle() {
+        return keysalle;
+    }
+
+    public void setKeysalle(Integer keysalle) {
+        this.keysalle = keysalle;
+    }
+
+    public Integer getKeydemandeformation() {
+        return keydemandeformation;
+    }
+
+    public void setKeydemandeformation(Integer keydemandeformation) {
+        this.keydemandeformation = keydemandeformation;
+    }
+
+    @Override
+    public String toString() {
+        return "FormationDTO{" + "idformation=" + idformation + ", intitule=" + intitule + ", nomclient=" + nomclient + ", nbpersonne=" + nbpersonne + ", statut=" + statut + ", dateformation=" + dateformation + ", keyformationcatalogue=" + keyformationcatalogue + ", keyformateur=" + keyformateur + ", keysalle=" + keysalle + ", keydemandeformation=" + keydemandeformation + '}';
+    }
+
 }
