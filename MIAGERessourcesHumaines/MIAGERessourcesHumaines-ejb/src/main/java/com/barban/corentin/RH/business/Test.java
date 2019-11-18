d8dc9307cc8bce173bb51051db4a76524826d952
julie <julie@DESKTOP-NSQ4CUF>
2019-11-18 10:41:53 +0100

    Ajout de la fonction edit statut de l'application RH

---
+++ MIAGERessourcesHumaines/MIAGERessourcesHumaines-ejb/src/main/java/com/barban/corentin/RH/business/Test.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.barban.corentin.RH.business;
+
+import Exceptions.FormateurNotFoundException;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.ejb.DependsOn;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ *
+ * @author Corentin
+ */
+@Startup
+@Singleton
+public class Test {
+
+    @EJB
+    private gestionRHLocal gestionRH;
+
+    public Test() {
+    }
+
+    @PostConstruct
+    void init() {
+        testEditStatutFormateur();
+    }
+
+    void testEditStatutFormateur() {
+        Date date = new Date();
+        try {
+            gestionRH.editStatutFormateur(1, "INDISPONIBLE", date);
+        } catch (FormateurNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+}
