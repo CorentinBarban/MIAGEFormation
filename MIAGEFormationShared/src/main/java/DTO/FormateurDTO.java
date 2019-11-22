8de6ec6f6562d73a186d3c456c0396907bce837b
julie <julie@DESKTOP-NSQ4CUF>
2019-11-22 11:50:22 +0100

    Ajout dans gestionRH la fonction verifierExistenceFormateur

---
+++ MIAGEFormationShared/src/main/java/DTO/FormateurDTO.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author julie
+ */
+public class FormateurDTO {
+
+    private int idFormateur;
+    private String nom;
+    private String prenom;
+    private Collection<CompetenceDTO> competenceCollection;
+
+    public int getIdFormateur() {
+        return idFormateur;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setIdFormateur(int idFormateur) {
+        this.idFormateur = idFormateur;
+    }
+    
+
+    public Collection<CompetenceDTO> getCompetenceCollection() {
+        return competenceCollection;
+    }
+
+    public void setCompetenceCollection(Collection<CompetenceDTO> competenceCollection) {
+        this.competenceCollection = competenceCollection;
+    }
+
+    @Override
+    public String toString() {
+        return "FormateurDTO{" + "idFormateur=" + idFormateur + ", nom=" + nom + ", prénom=" + prenom + 
+                ", competenceCollection=" + competenceCollection + '}';
+    }
+   
+
+}
