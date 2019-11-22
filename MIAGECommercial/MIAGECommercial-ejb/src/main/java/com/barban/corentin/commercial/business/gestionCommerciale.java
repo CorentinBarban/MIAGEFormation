fd8ed3b2498362e5ec45c4757cdf3df2c6060a56
maths <math.stiv@gmail.com>
2019-11-22 14:02:04 +0100

    Code métier temporaire

--- MIAGECommercial/MIAGECommercial-ejb/src/main/java/com/barban/corentin/commercial/business/gestionCommerciale.java
+++ MIAGECommercial/MIAGECommercial-ejb/src/main/java/com/barban/corentin/commercial/business/gestionCommerciale.java
@@ -10,52 +10,93 @@ import DTO.FormationDTO;
 import Exceptions.ListeFormationsVideException;
 import com.barban.corentin.commercial.entities.Demandedeformation;
 import com.barban.corentin.commercial.repositories.DemandedeformationFacadeLocal;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 /**
  * Classe métier de la gestion commerciale
+ *
  * @author Mathieu Stivanin
  */
-@Stateless
-public class gestionCommerciale implements gestionCommercialeLocal {
+
+    @Stateless
+    public class gestionCommerciale implements gestionCommercialeLocal {
 
     @EJB
     private DemandedeformationFacadeLocal demandedeformationFacade;
-/**
- * Méthode permettant de récupérer le catalogue de formations =.
- * @return Une liste de formations DTO
- */
+
+    /**
+     * Méthode permettant de récupérer le catalogue de formations.
+     *
+     * @return Une liste de formations DTO ??
+     */
+    
     @Override
     public List<FormationDTO> recupererCatalogueFormations() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        try {
+            URL url = new URL("/formationsCatalogue");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "+ conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            System.out.println("Liste des formations présentes dans la catalogue : \n"); // Ici, la récupération du catalogueFormation sera faite lorsque ce sera implémenté côté TC
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();            
+        } catch (IOException ex) {
+            Logger.getLogger(gestionCommerciale.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
     }
 
     /**
      * Méthode permettant de mémoriser une demande de formation
+     *
      * @param nomClient
      * @param dateDemande
      * @param codeFormation
      * @param intituleFormation
-     * @param codeclient 
+     * @param codeclient
      */
+    
     @Override
     public void memoriserDemandeFormation(String nomClient, Date dateDemande, String codeFormation, String intituleFormation, int codeclient) {
         Demandedeformation demandeFormation = new Demandedeformation(nomClient, dateDemande, codeFormation, intituleFormation, codeclient);
         Demandedeformation dfObjet = this.demandedeformationFacade.create(demandeFormation);
     }
-    
+
     /**
      * Méthode permettant de générer des comptes rendus, positifs ou négatifs.
+     *
      * @return Une objet CompteRenduDTO représentant un compte rendu
-     * @throws ListeFormationsVideException 
+     * @throws ListeFormationsVideException
      */
-
+    
     @Override
     public CompteRenduDTO editerCompteRendus() throws ListeFormationsVideException { //Doit throw aussi CapaciteNotFound
         List<FormationDTO> listeFormations = new ArrayList<FormationDTO>();
@@ -71,7 +112,6 @@ public class gestionCommerciale implements gestionCommercialeLocal {
                 Date dateJour30 = calendier.getTime();
                 Date jour = new Date();
                 //Ici, on doit aller taper dans technico-commercial pour chopper la clé de la formation en question dans le formationcatalogue pour avoir la capacite (REST)
-                //Récupérer keycatalogue de la formation, check dans le REST de TC pour avoir la capacité correspondante
 
                 if (nbPersonnes < capaciteMin && jour == dateJour30) {
                     compteRendu = new CompteRenduDTO(formation.getIntitule(), formation.getDateformation(), formation.getNomclient(), "Négatif", formation.getNbpersonne());
@@ -84,23 +124,26 @@ public class gestionCommerciale implements gestionCommercialeLocal {
         }
         return compteRendu;
     }
-/**
- * Fonction permettant de valider l'existence d'une formation
- * @param code code de la formation
- * @return true si la formation existe, false si la formation n'existe pas
- */
+
+    /**
+     * Fonction permettant de valider l'existence d'une formation
+     *
+     * @param code code de la formation
+     * @return true si la formation existe, false si la formation n'existe pas
+     */
     @Override
     public boolean validerExistenceFormation(int code) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        //Appel REST de la même méthode chez TC
+        //Récupérer toutes les formations en REST via le catalogue, et vérifier si elle existe. Si elle existe, return true.
     }
-    
+
     /**
      * Méthode permettant de demander l'état d'une formation
+     *
      * @param idFormation l'id de la formation souhaitée
      * @return une chaine contenant l'état de la formation
      */
-
+    
     @Override
     public String demanderEtatFormation(int idFormation) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
